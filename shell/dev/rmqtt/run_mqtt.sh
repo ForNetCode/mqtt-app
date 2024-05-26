0af3dd0c31bf53155fbe01f731a5ad13481b88b0
timzaak <zsy.evan@gmail.com>
2024-05-26 22:28:30 +0800

    fix: bash

--- shell/dev/rmqtt/run_mqtt.sh
+++ shell/dev/rmqtt/run_mqtt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker run -d --name rmqtt -p 6060:6060 -p 1883:1883 -p 8080:8080 \
--v $(pwd)/rmqtt/rmqtt.toml:/app/rmqtt/rmqtt.toml \
--v $(pwd)/rmqtt/plugin:/app/rmqtt/plugin  \
+-v $(pwd)/rmqtt.toml:/app/rmqtt/rmqtt.toml \
+-v $(pwd)/plugin:/app/rmqtt/plugin  \
 rmqtt/rmqtt:latest
 
 
